f514014db97d2e63209000ec9ad2a6131b93e001
nplachkov <nikito12386@gmail.com>
2023-11-09 21:48:17 +0200

    Cleanup

--- src/test/java/tests/AddToCartProblemUserTest.java
+++ src/test/java/tests/AddToCartProblemUserTest.java
@@ -19,8 +19,33 @@ public class AddToCartProblemUserTest extends TestUtil {
         ProductPage productPage = new ProductPage(driver);
 
         productPage.addItemToCart("backpack");
+        int afterItem1 = productPage.getItemsInCart();
+
         productPage.addItemToCart("bike-light");
+        int afterItem2 = productPage.getItemsInCart();
+
         productPage.addItemToCart("bolt-t-shirt");
+        int afterItem3 = productPage.getItemsInCart();
+
+        //A simple if statement, to understand which item was not added successfully - if any.
+        if (afterItem1 > 0){
+            System.out.println("Successfully added item 1.");
+            if (afterItem2 == afterItem1+1){
+                System.out.println("Successfully added item 2.");
+                if (afterItem3 == afterItem2+1){
+                    System.out.println("Successfully added item 3.");
+                }
+                else {
+                    System.out.println("Item 3 was not added.");
+                }
+            }
+            else {
+                System.out.println("Item 2 was not added.");
+            }
+        }
+        else {
+            System.out.println("Item 1 was not added.");
+        }
 
         Assert.assertEquals(productPage.getItemsInCart(),3,"Amount of items added to cart.");
     }
